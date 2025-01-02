10fd2d332ddad0d9f21bde8bed182611fe98886d
unchalante <jonathanchilongani@gmail.com>
2025-01-03 00:27:39 +0300

    bills

--- marketing.cpp
+++ marketing.cpp
@@ -7,3 +7,13 @@ using namespace std;
 class Bill{
 private:
  string Item;
+ int Rate, Quantity;
+public:
+ Bill():Item(""), Rate(0), Quantity(0){ }
+ 
+ void setItem(string item){
+ Item = item;
+ }
+ 
+void setRate(int rate){
+ Rate = rate;
